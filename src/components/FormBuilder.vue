66fc162395c8c4972f1f339243cf263eb325d228
Brock <brock.reece@croud.co.uk>
2017-08-03 17:19:54 +0100

    Add Form builder component

---
+++ src/components/FormBuilder.vue
@@ -0,0 +1,72 @@
+<script>
+    export default {
+        props: {
+            value: {
+                required: true,
+            },
+            schema: {
+                required: true,
+            },
+
+            readOnly: {
+                type: Boolean,
+                default: false,
+            },
+        },
+
+        render(h) {
+            const schema = this.schema.map((field) => {
+                if (field.children) {
+                    return this.recur(h, field)
+                }
+
+                return this.buildNode(h, field)
+            })
+
+            return h('div', {
+                class: 'ui form',
+            }, schema)
+        },
+
+        methods: {
+            buildNode(h, field) {
+                return h('croud-form-field', {
+                    props: {
+                        field,
+                        model: this.value[field.field_slug],
+                        readOnly: this.readOnly,
+                    },
+
+                    on: {
+                        'set-value': (val) => {
+                            this.value[field.field_slug] = val
+                        },
+                    },
+                })
+            },
+
+            recur(h, field) {
+                const children = field.children.map((child) => {
+                    if (child.children) {
+                        return this.recur(h, child)
+                    }
+
+                    return this.buildNode(h, child)
+                })
+
+                return h('div', {
+                    class: field.class,
+                }, children)
+            },
+        },
+
+        watch: {
+            value: {
+                deep: true,
+                handler(val) {
+                    this.$emit('input', val)
+                },
+            },
+        },
+    }
+</script>
--- src/croud-forms.js
+++ src/croud-forms.js
@@ -1,3 +1,4 @@
+const FormBuilder = require('./components/FormBuilder.vue')
 const FormRow = require('./components/FormRow.vue')
 const FormField = require('./components/FormField.vue')
 const DateTime = require('./components/fields/DateTime.vue')
@@ -6,6 +7,7 @@ const Time = require('./components/fields/Time.vue')
 
 module.exports = {
     install(Vue) {
+        Vue.component('croud-form-builder', FormBuilder)
         Vue.component('croud-form-field', FormField)
         Vue.component('croud-form-row', FormRow)
         Vue.component('croud-date-time', DateTime)
